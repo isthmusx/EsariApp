fba69d8fb3c301ac8d366ed64cc34dc2bbd09946
John Paul Al-haddi <jpaul.alhaddi@gmail.com>
2022-11-23 18:34:15 +0800

    Add project files.

---
+++ EsariApp/EsariApp/LoginUI.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EsariApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+
+          
+
+        }
+
+        private void BTNLogin(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtPassword.Text == "admin123")
+            {
+                Navigation.PushAsync(new Homepage());
+            }
+            else
+            {
+                DisplayAlert("Oops..", "Your credentials are incorrect!", "Ok");
+            }
+        }
+
+        private void BTNRegister(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+
+        private void BTNForgotPass(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ForgotPassPage());
+        }
+    }
+}
